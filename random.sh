ce0b99a434dbb955ed44717f6bc0130a1a059787
Eric Schultz <erschultz@uchicagoe.edu>
2021-08-17 22:53:27 -0500

    update

--- random.sh
+++ random.sh
@@ -40,7 +40,7 @@ source activate python3.8_pytorch1.8.1_cuda10.2
 for i in $(seq $startSimulation $(($startSimulation + $numSimulations - 1)))
 do
   # set up config.json
-	python3 ~/TICG-chromatin/scripts/get_config.py --save_chi --chi=$chi --m $m --min_chi $minChi --max_chi $maxChi --fill_diag $fillDiag --ensure_distinguishable > log.log
+	python3 ~/TICG-chromatin/scripts/get_config.py --save_chi --chi=$chi --m $m --k $k --min_chi $minChi --max_chi $maxChi --fill_diag $fillDiag --ensure_distinguishable > log.log
 
 	# generate sequences
 	python3 ~/TICG-chromatin/scripts/get_seq.py --method $method --m $m --p_switch $pSwitch --k $k >> log.log
