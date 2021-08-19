770aeab68589398c6dcbfeb75101486514d8c880
Eric Schultz <erschultz@uchicagoe.edu>
2021-08-19 14:36:02 -0500

    more bash setup

--- random.sh
+++ random.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-sbatch random1.sh
-sbatch random2.sh
-sbatch random3.sh
-sbatch random4.sh
-sbatch random5.sh
-sbatch random6.sh
+sbatch bin/random7.sh
+sbatch bin/random8.sh
+sbatch bin/random9.sh
+sbatch bin/random10.sh
