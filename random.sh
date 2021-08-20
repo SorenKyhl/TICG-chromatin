0377918e52ca4801f0a89995b9f8091965979622
Eric Schultz <erschultz@uchicagoe.edu>
2021-08-20 09:43:03 -0500

    update

--- random.sh
+++ random.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
+sbatch bin/random1.sh
+sbatch bin/random2.sh
+sbatch bin/random3.sh
+sbatch bin/random4.sh
+sbatch bin/random5.sh
+sbatch bin/random6.sh
 sbatch bin/random7.sh
 sbatch bin/random8.sh
 sbatch bin/random9.sh
 sbatch bin/random10.sh
+sbatch bin/random11.sh
