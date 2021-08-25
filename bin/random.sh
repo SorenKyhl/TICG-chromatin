2edc20d7427cade73edddea2f7cf9b8526e3e650
Eric Schultz <erschultz@uchicagoe.edu>
2021-08-25 13:09:58 -0500

    update

--- bin/random.sh
+++ bin/random.sh
@@ -6,7 +6,7 @@ k=2
 dataFolder="/project2/depablo/erschultz/dataset_08_24_21"
 
 cd ~/TICG-chromatin
-# sbatch bin/random1.sh 1 50 $k $chi $dataFolder
+sbatch bin/random1.sh 1 50 $k $chi $dataFolder
 # sbatch bin/random2.sh 51 100 $k $chi $dataFolder
 # sbatch bin/random3.sh 101 150 $k $chi $dataFolder
 # sbatch bin/random4.sh 151 200 $k $chi $dataFolder
@@ -15,10 +15,10 @@ cd ~/TICG-chromatin
 # sbatch bin/random7.sh 301 350 $k $chi $dataFolder
 # sbatch bin/random8.sh 351 400 $k $chi $dataFolder
 # sbatch bin/random9.sh 401 450 $k $chi $dataFolder
-sbatch bin/random10.sh 451 500 $k $chi $dataFolder
-sbatch bin/random11.sh 501 550 $k $chi $dataFolder
-sbatch bin/random12.sh 551 600 $k $chi $dataFolder
-sbatch bin/random13.sh 601 650 $k $chi $dataFolder
-sbatch bin/random14.sh 651 700 $k $chi $dataFolder
-sbatch bin/random15.sh 701 750 $k $chi $dataFolder
-sbatch bin/random16.sh 751 800 $k $chi $dataFolder
+# sbatch bin/random10.sh 451 500 $k $chi $dataFolder
+# sbatch bin/random11.sh 501 550 $k $chi $dataFolder
+# sbatch bin/random12.sh 551 600 $k $chi $dataFolder
+# sbatch bin/random13.sh 601 650 $k $chi $dataFolder
+# sbatch bin/random14.sh 651 700 $k $chi $dataFolder
+# sbatch bin/random15.sh 701 750 $k $chi $dataFolder
+# sbatch bin/random16.sh 751 800 $k $chi $dataFolder
