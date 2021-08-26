2aabf99f940e5271208bc3411ac943d976e999f1
Eric Schultz <erschultz@uchicagoe.edu>
2021-08-26 15:19:00 -0500

    update

--- bin/random.sh
+++ bin/random.sh
@@ -18,7 +18,7 @@ startSample=1
 
 for i in $(seq 1 16)
 do
-  start=$(( ($i-1)*$samplesPerTask)+$startSample ))
+  start=$(( $(( $(( $i-1 ))*$samplesPerTask ))+$startSample ))
   stop=$($start+$samplesPerTask)
   echo $start $stop
   # ~/TICG-chromatin/bin/random_inner.sh $i $k $chi $m $start $stop $dataFolder & >> TICG${i}.log
