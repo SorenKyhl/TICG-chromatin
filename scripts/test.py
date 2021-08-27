c39a631d1d9d413c295d0695f2748160b4c9807d
ERSchultz <schultz-eric@hotmail.com>
2021-08-27 13:09:54 -0500

    update

--- scripts/test.py
+++ scripts/test.py
@@ -1,15 +1,15 @@
 import sys
+import os
 import os.path as osp
+
 import numpy as np
 
 def main():
-    x = np.load("../sequences_to_contact_maps/dataset_04_18_21/samples/sample40/x.npy")
-    print(x)
-
-    x0 = np.loadtxt("../sequences_to_contact_maps/dataset_04_18_21/samples/sample40/ground_truth2/k2/resources/seq0.txt")
-    x1 = np.loadtxt("../sequences_to_contact_maps/dataset_04_18_21/samples/sample40/ground_truth2/k2/resources/seq1.txt")
-    print(np.array_equal(x[:, 0] , x0))
-    print(np.array_equal(x[:, 1] , x1))
+    dir = "/project2/depablo/erschultz/dataset_08_24_21/samples"
+    for file in os.listdir(dir):
+        if file.startswith('sample'):
+            if not osp.exists(osp.join(dir, file, 'seq0.txt')):
+                print(file)
 
 if __name__ == '__main__':
     main()
