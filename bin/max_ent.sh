2556f786e866e0ea8209379ae76cab48ebadb93a
ERSchultz <schultz-eric@hotmail.com>
2021-09-10 14:42:43 -0500

    update

--- bin/max_ent.sh
+++ bin/max_ent.sh
@@ -13,7 +13,7 @@ dataFolder='/project2/depablo/erschultz/dataset_08_26_21'
 productionSweeps=50000
 equilibSweeps=10000
 goalSpecified=1
-numIterations=5 # iteration 1 + numIterations is production run to get contact map
+numIterations=50 # iteration 1 + numIterations is production run to get contact map
 overwrite=1
 scratchDir='/scratch/midway2/erschultz/TICG'
 
@@ -22,7 +22,19 @@ module load jq
 
 STARTTIME=$(date +%s)
 #'GNN' 'ground_truth' 'random' 'k_means' 'PCA' 'PCA_split' 'nmf'
-for method in 'GNN' 'ground_truth' 'random' 'k_means' 'PCA' 'PCA_split' 'nmf'
+for method in 'ground_truth' 'random' 'k_means' 'PCA' 'PCA_split' 'nmf'
+do
+  ~/TICG-chromatin/bin/max_ent_inner.sh $m $k $sample $dataFolder $productionSweeps $equilibSweeps $goalSpecified $numIterations $overwrite "${scratchDir}_${method}" $method > ~/TICG-chromatin/logFiles/TICG_${method}.log &
+done
+
+k=2
+for method in 'random' 'k_means' 'PCA' 'PCA_split' 'nmf'
+do
+  ~/TICG-chromatin/bin/max_ent_inner.sh $m $k $sample $dataFolder $productionSweeps $equilibSweeps $goalSpecified $numIterations $overwrite "${scratchDir}_${method}" $method > ~/TICG-chromatin/logFiles/TICG_${method}.log &
+done
+
+k=6
+for method in 'random' 'k_means' 'PCA' 'PCA_split' 'nmf'
 do
   ~/TICG-chromatin/bin/max_ent_inner.sh $m $k $sample $dataFolder $productionSweeps $equilibSweeps $goalSpecified $numIterations $overwrite "${scratchDir}_${method}" $method > ~/TICG-chromatin/logFiles/TICG_${method}.log &
 done
