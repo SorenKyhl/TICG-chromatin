ba3fc008121278e35bac23ef5fa2449bebf449b1
Eric Schultz <erschultz@uchicagoe.edu>
2021-10-05 09:23:48 -0500

    init commit make_latex_table.sh

---
+++ bin/make_latex_table.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#SBATCH --job-name=latex
+#SBATCH --output=latex.out
+#SBATCH --time=24:00:00
+#SBATCH --partition=depablo-ivyb
+#SBATCH --ntasks=20
+#SBATCH --mem-per-cpu=2000
+
+samples=1
+dataFolder='/project2/depablo/erschultz/dataset_09_21_21'
+
+source activate python3.8_pytorch1.8.1_cuda10.2
+
+python3 ~/TICG-chromatin/scripts/makeLatexTable.py --data_folder $dataFolder --samples $samples
