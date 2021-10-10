fc2e63f2aa72e08ea24f472a15be21804c42c5a1
ERSchultz <schultz-eric@hotmail.com>
2021-10-10 17:04:11 -0500

    update

--- scripts/contact_map.py
+++ scripts/contact_map.py
@@ -18,6 +18,7 @@ for p in paths:
 
 from neural_net_utils.utils import diagonal_preprocessing
 from data_summary_plots import genomic_distance_statistics
+from plotting_functions import plotContactMap
 
 def getArgs():
     parser = argparse.ArgumentParser(description='Base parser')
@@ -28,47 +29,6 @@ def getArgs():
 
     args = parser.parse_args()
     return args
-
-def plotContactMap(y, ofile = None, title = None, vmin = 0, vmax = 1, size_in = 6, minVal = None, maxVal = None, cmap = None):
-    """
-    Plotting function for contact maps.
-
-    Inputs:
-        y: contact map numpy array
-        ofile: save location
-        title: plot title
-        vmax: maximum value for color bar, 'mean' to set as mean value
-        size_in: size of figure x,y in inches
-        minVal: values in y less than minVal are set to 0
-        maxVal: values in y greater than maxVal are set to 0
-    """
-    if cmap is None:
-        cmap = matplotlib.colors.LinearSegmentedColormap.from_list('custom',
-                                                 [(0,    'white'),
-                                                  (1,    'red')], N=126)
-    if minVal is not None:
-        ind = y < minVal
-        y[ind] = 0
-    if maxVal is not None:
-        ind = y > maxVal
-        y[ind] = 0
-    plt.figure(figsize = (size_in, size_in))
-    if vmax == 'mean':
-        vmax = np.mean(y)
-    elif vmax == 'max':
-        vmax = np.max(y)
-    if vmin == 'min':
-        vmin = np.min(y)
-    ax = sns.heatmap(y, linewidth = 0, vmin = vmin, vmax = vmax, cmap = cmap)
-    if title is not None:
-        plt.title(title, fontsize = 16)
-    plt.tight_layout()
-    if ofile is not None:
-        plt.savefig(ofile)
-    else:
-        plt.show()
-    plt.close()
-
 
 def main():
     args = getArgs()
