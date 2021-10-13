0f17e7b723d1249a994c03de89d9603ccdea4197
Eric Schultz <erschultz@uchicagoe.edu>
2021-10-13 10:28:14 -0500

    update

--- bin/make_latex_table.sh
+++ bin/make_latex_table.sh
@@ -14,7 +14,7 @@ source activate python3.8_pytorch1.8.1_cuda10.2
 
 python3 ~/TICG-chromatin/scripts/makeLatexTable.py --data_folder $dataFolder --samples $samples --small $small
 
-samples="40-1230"
+samples="40-1230-1718"
 dataFolder='/project2/depablo/erschultz/dataset_08_29_21'
 python3 ~/TICG-chromatin/scripts/makeLatexTable.py --data_folder $dataFolder --samples $samples --small $small
 python3 ~/TICG-chromatin/scripts/makeLatexTable.py --data_folder $dataFolder --samples $samples
