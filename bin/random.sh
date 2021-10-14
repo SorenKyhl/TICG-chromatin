ee3f4715f2d8bba32639cdb51f5287199d7732a5
Eric Schultz <erschultz@uchicagoe.edu>
2021-10-14 13:30:20 -0500

    update

--- bin/random.sh
+++ bin/random.sh
@@ -7,17 +7,17 @@
 #SBATCH --mem-per-cpu=2000
 
 # chi="-1&2&-1&1.5\\2&-1&-1&-0.5\\-1&-1&-1&1.5\\1.5&-0.5&1.5&-1"
-chi="-1&1&0&0\\1&-2&0&-1\\0&0&-1&2\\0&-1&2&-1"
-# chi="-1&1\\1&0"
+# chi="-1&1&0&0\\1&-2&0&-1\\0&0&-1&2\\0&-1&2&-1"
+chi="-1&1\\1&0"
 # chi='none'
-k=4
+k=2
 m=1024
 today=$(date +'%m_%d_%y')
 dataFolder="/project2/depablo/erschultz/dataset_10_14_21"
 samplesPerTask=40
 startSample=1
 relabel='none'
-diag='false'
+diag='true'
 
 cd ~/TICG-chromatin/src
 make
