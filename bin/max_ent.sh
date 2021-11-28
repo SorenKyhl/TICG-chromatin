961640a93b60046c9e5ac4f350f5d110f5e4bdd6
Eric Schultz <erschultz@uchicagoe.edu>
2021-11-28 13:50:28 -0600

    update

--- bin/max_ent.sh
+++ bin/max_ent.sh
@@ -161,16 +161,16 @@ i=1
 #   max_ent
 # done
 #
-dataFolder='/project2/depablo/erschultz/dataset_08_29_21'
-useGroundTruthChi='true'
-method='ground_truth'
-numIterations=0
-goalSpecified='false'
-k=2
-for sample in 40
-do
-  max_ent
-done
+# dataFolder='/project2/depablo/erschultz/dataset_08_29_21'
+# useGroundTruthChi='true'
+# method='ground_truth'
+# numIterations=0
+# goalSpecified='false'
+# k=2
+# for sample in 40
+# do
+#   max_ent
+# done
 #
 # useGroundTruthChi='false'
 # method='ground_truth'
@@ -197,19 +197,19 @@ done
 #     max_ent
 # done
 
-# dataFolder='/project2/depablo/erschultz/dataset_08_29_21'
-# k='none'
-# useEnergy='true'
-# goalSpecified='false'
-# useGroundTruthChi='false'
-# numIterations=0
-# correctEnergy='true'
-# modelID=55
-# method='GNN'
-# for sample in 40 1230 1718
-# do
-#     max_ent
-# done
+dataFolder='/project2/depablo/erschultz/dataset_08_29_21'
+k='none'
+useEnergy='true'
+goalSpecified='false'
+useGroundTruthChi='false'
+numIterations=0
+correctEnergy='false'
+modelID=55
+method='GNN'
+for sample in 40 1230 1718
+do
+    max_ent
+done
 #
 # k='none'
 # useEnergy='true'
