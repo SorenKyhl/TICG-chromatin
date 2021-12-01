f7c1a5361cf8f78c01b1a83f07ee5c70868aeb94
ERSchultz <schultz-eric@hotmail.com>
2021-11-30 20:03:09 -0600

    update

--- bin/latex.sh
+++ bin/latex.sh
@@ -19,16 +19,16 @@ source activate python3.8_pytorch1.8.1_cuda10.2
 # python3 ~/TICG-chromatin/scripts/makeLatexTable.py --data_folder $dataFolder --samples $samples
 
 samples="40-1230-1718"
-# dataFolder='dataset_08_24_21'
-# dataFolder='dataset_08_26_21'
-# dataFolder='dataset_08_29_21'
-dataFolder='dataset_10_27_21'
-# dataFolder='/dataset_11_03_21'
-# dataFolder='dataset_11_14_21'
+# dataSet='dataset_08_24_21'
+# dataSet='dataset_08_26_21'
+# dataSet='dataset_08_29_21'
+dataSet='dataset_10_27_21'
+# dataSet='/dataset_11_03_21'
+# dataSet='dataset_11_14_21'
 dataDir='/project2/depablo/erschultz'
 
 for dataset in dataset_10_27_21
 do
-  dataFolder = "${dataDir}/${dataset}"
+  dataFolder="${dataDir}/${dataset}"
   python3 ~/TICG-chromatin/scripts/makeLatexTable.py --data_folder $dataFolder --samples $samples
 done
