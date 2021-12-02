92721745b169413f1d6bb959a150e78f5809eb69
ERSchultz <schultz-eric@hotmail.com>
2021-12-02 17:50:01 -0600

    update

--- bin/latex.sh
+++ bin/latex.sh
@@ -23,11 +23,11 @@ samples="40-1230-1718"
 # dataSet='dataset_08_26_21'
 # dataSet='dataset_08_29_21'
 # dataSet='dataset_10_27_21'
-dataSet='/dataset_11_03_21'
+dataSet='dataset_11_03_21'
 # dataSet='dataset_11_14_21'
 dataDir='/project2/depablo/erschultz'
 
-for dataset in dataset_10_27_21
+for dataset in dataset_11_03_21
 do
   dataFolder="${dataDir}/${dataset}"
   python3 ~/TICG-chromatin/scripts/makeLatexTable.py --data_folder $dataFolder --samples $samples
