38e3198bb23142a6e8d82228653589ab2ad5d90a
ERSchultz <schultz-eric@hotmail.com>
2021-12-17 17:29:20 -0600

    update

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem-per-cpu=2000
 #SBATCH --qos=depablo-debug
 
-cd /project2/depablo/erschultz/dataset_12_11_21
+cd /project2/depablo/erschultz/dataset_12_11_21/samples
 cd sample40
 rm -r ground_truth*
 
@@ -17,7 +17,7 @@ rm -r ground_truth*
 cd ../sample1718
 rm -r ground_truth*
 
-cd /project2/depablo/erschultz/dataset_12_12_21
+cd /project2/depablo/erschultz/dataset_12_12_21/samples
 cd sample40
 rm -r ground_truth* GNN* PCA* kPCA* nmf* random* k_means*
 
