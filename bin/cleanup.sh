cc1b54eb2bf89a72138a12e47b560997f55a4980
Eric Schultz <erschultz@uchicagoe.edu>
2021-12-21 22:58:45 -0600

    update

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -10,14 +10,17 @@ cd /project2/depablo/erschultz/dataset_12_17_21/samples
 cd sample40
 rm -r ground_truth-E &
 rm -r ground_truth-S &
+rm -r ground_truth* &
 wait
 
 cd ../sample1230
 rm -r ground_truth-E &
 rm -r ground_truth-S &
+rm -r ground_truth* &
 wait
 
 cd ../sample1718
 rm -r ground_truth-E &
 rm -r ground_truth-S &
+rm -r ground_truth* &
 wait
