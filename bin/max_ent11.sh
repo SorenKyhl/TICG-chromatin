22f4d3eda199e515e337eaf160f81045fb0b1dc5
Eric Schultz <erschultz@uchicagoe.edu>
2022-01-03 16:28:57 -0600

    update

---
+++ bin/max_ent11.sh
@@ -0,0 +1,71 @@
+#! /bin/bash
+#SBATCH --job-name=maxent10
+#SBATCH --output=logFiles/maxent10.out
+#SBATCH --time=24:00:00
+#SBATCH --partition=depablo-ivyb
+#SBATCH --ntasks=20
+#SBATCH --mem-per-cpu=2000
+
+m=1024
+k='none'
+samples='40-1230-1718'
+productionSweeps=50000
+finalSimProductionSweeps=1000000
+equilibSweeps=10000
+goalSpecified='true'
+numIterations=100 # iteration 1 + numIterations is production run to get contact map
+overwrite=1
+modelType='ContactGNNEnergy'
+local='false'
+useE='false'
+useS='false'
+useGroundTruthChi='false'
+useGroundTruthDiagChi='true'
+useGroundTruthSeed='false'
+mode="plaid"
+gamma=0.00001
+gammaDiag=0.00001
+resources=~/TICG-chromatin/maxent/resources
+chipSeqFolder="/home/erschultz/sequences_to_contact_maps/chip_seq_data"
+epiData="${chipSeqFolder}/fold_change_control/processed"
+chromHMMData="${chipSeqFolder}/aligned_reads/ChromHMM_15/STATEBYLINE/HTC116_15_chr2_statebyline.txt"
+results=~/sequences_to_contact_maps/results
+
+source ~/TICG-chromatin/bin/max_ent_fns.sh
+
+if [ $local = 'true' ]
+then
+  dir="/home/eric/sequences_to_contact_maps"
+  dir='C:\\Users\\Eric\\OneDrive\\Documents\\Research\\Coding\\sequences_to_contact_maps'
+  # dataFolder="/home/eric/dataset_test"
+  finalSimProductionSweeps=1000
+  numIterations=0
+  scratchDir='/home/eric/scratch'
+  scratchDir='C:\\Users\\Eric\\OneDrive\\Documents\\Research\\Coding\\scratch'
+  source activate python3.8_pytorch1.8.1_cuda11.1
+else
+  dir='/project2/depablo/erschultz'
+  scratchDir='/scratch/midway2/erschultz'
+  source activate python3.8_pytorch1.8.1_cuda10.2_2
+fi
+
+STARTTIME=$(date +%s)
+i=9000
+dataset='dataset_12_29_21'
+method='k_means'
+for k in 4 6
+do
+  for sample in 40 1230 1718
+  do
+    max_ent
+  done
+done
+
+
+wait
+
+python3 ~/TICG-chromatin/scripts/makeLatexTable.py --data_folder $dataFolder --samples $samples
+# python3 ~/TICG-chromatin/scripts/makeLatexTable.py --data_folder $dataFolder --samples $samples --small "true"
+
+ENDTIME=$(date +%s)
+echo "total time:$(( $(( $ENDTIME - $STARTTIME )) / 60 )) minutes"
