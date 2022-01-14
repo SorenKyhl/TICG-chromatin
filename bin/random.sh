c425c672c1d458f03bcce607b9fb49588d58c624
ERSchultz <schultz-eric@hotmail.com>
2022-01-13 20:13:52 -0600

    job

--- bin/random.sh
+++ bin/random.sh
@@ -8,12 +8,12 @@ k=4
 m=1024
 today=$(date +'%m_%d_%y')
 dataFolder="/project2/depablo/erschultz/dataset_01_15_22"
-startSample=1
+startSample=-619
 relabel='none'
-startNode=0
-nodes=4
+startNode=4
+nodes=6
 tasks=20
-samples=80
+samples=1200
 diag='true'
 nSweeps=1000000
 pSwitch=0.05
@@ -55,5 +55,5 @@ do
   startSampleI=$(( $startSample + $samplesPerNode * $i ))
   endSampleI=$(( $startSampleI + $samplesPerNode - 1 ))
   echo "TICG${i}" $startSampleI $endSampleI
-  sbatch ~/TICG-chromatin/bin/random${i}.sh $chi $k $m $dataFolder $startSampleI $relabel $tasks $samplesPerNode $samplesPerTask $diag $scratchDir $i $nSweeps $pSwitch $minChi $maxChi $fillDiag $chiSeed $maxDiagChi
+  # sbatch ~/TICG-chromatin/bin/random${i}.sh $chi $k $m $dataFolder $startSampleI $relabel $tasks $samplesPerNode $samplesPerTask $diag $scratchDir $i $nSweeps $pSwitch $minChi $maxChi $fillDiag $chiSeed $maxDiagChi
 done
