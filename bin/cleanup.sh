32faa853c9c078d4c5dc438507013391a46dba7f
ERSchultz <schultz-eric@hotmail.com>
2022-01-19 18:44:34 -0600

    big cleanup

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -6,9 +6,60 @@
 #SBATCH --ntasks=10
 #SBATCH --mem-per-cpu=1000
 
-cd /project2/depablo/erschultz/dataset_01_19_22/samples
+cd /project2/depablo/erschultz/dataset_10_27_21/samples
 
-for i in $( seq 2001 2100 )
-do
-  rm -r "sample${i}"
-done
+cd sample40
+rm -r random &
+rm -r PCA* &
+rm -r kPCA* &
+rm -r k_means &
+rm -r ground* &
+rm -r GNN* &
+rm -r nmf* &
+
+cd ../sample1230
+rm -r random &
+rm -r PCA* &
+rm -r kPCA* &
+rm -r k_means &
+rm -r ground* &
+rm -r GNN* &
+rm -r nmf* &
+
+cd ../sample1718
+rm -r random &
+rm -r PCA* &
+rm -r kPCA* &
+rm -r k_means &
+rm -r ground* &
+rm -r GNN* &
+rm -r nmf* &
+
+cd /project2/depablo/erschultz/dataset_11_03_21/samples
+
+cd sample40
+rm -r random &
+rm -r PCA* &
+rm -r kPCA* &
+rm -r k_means &
+rm -r ground* &
+rm -r GNN* &
+rm -r nmf* &
+
+cd ../sample1230
+rm -r random &
+rm -r PCA* &
+rm -r kPCA* &
+rm -r k_means &
+rm -r ground* &
+rm -r GNN* &
+rm -r nmf* &
+
+cd ../sample1718
+rm -r random &
+rm -r PCA* &
+rm -r kPCA* &
+rm -r k_means &
+rm -r ground* &
+rm -r GNN* &
+rm -r nmf* &
