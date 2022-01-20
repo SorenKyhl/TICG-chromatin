b86e4484953d8f69500c8fba840f7d82e610f582
ERSchultz <schultz-eric@hotmail.com>
2022-01-19 18:18:35 -0600

    job

--- bin/latex.sh
+++ bin/latex.sh
@@ -19,7 +19,7 @@ source activate python3.8_pytorch1.8.1_cuda10.2
 # samples="11-13"
 # python3 ~/TICG-chromatin/scripts/makeLatexTable.py --data_folder $dataFolder --samples $samples
 
-samples="40-1718-1751-1761"
+samples="40-1230-1718"
 sample=40
 # dataSet='dataset_08_24_21'
 # dataSet='dataset_08_26_21'
@@ -31,11 +31,11 @@ dataDir='/project2/depablo/erschultz'
 # dataDir='/home/eric/sequences_to_contact_maps'
 
 
-dataset=dataset_01_19_22
+dataset=dataset_10_27_21
 dataFolder="${dataDir}/${dataset}"
 python3 ~/TICG-chromatin/scripts/makeLatexTable.py --data_folder $dataFolder --samples $samples
 
-for sample in 40 1718
+for sample in 40 1230 1718
 do
   python3 ~/TICG-chromatin/scripts/makeLatexTable.py --data_folder $dataFolder --sample $sample
 done
