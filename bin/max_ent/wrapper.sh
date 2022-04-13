7ad32ffcf317f66bba0e1233ee1d62e19ce57fb5
ERSchultz <schultz-eric@hotmail.com>
2022-04-13 12:00:59 -0500

    job

--- bin/max_ent/wrapper.sh
+++ bin/max_ent/wrapper.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+source activate python3.9_pytorch1.9_cuda10.2
 module unload gcc
 module load gcc/10.2.0
 
@@ -11,7 +12,7 @@ mv TICG-engine ..
 # necessary to ensure log files are in right place
 cd ~/TICG-chromatin
 
-for i in 5
+for i in 4
 do
   echo $i
   sbatch ~/TICG-chromatin/bin/max_ent/max_ent${i}.sh
