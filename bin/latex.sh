cef0843ba8b255d6ccfb41c6917a8ca51b21fb36
ERSchultz <schultz-eric@hotmail.com>
2022-04-14 11:33:12 -0500

    job

--- bin/latex.sh
+++ bin/latex.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem-per-cpu=4000
 #SBATCH --qos=depablo-debug
 
-source activate python3.8_pytorch1.8.1_cuda10.2_2
+source activate python3.9_pytorch1.9_cuda10.2
 dataDir='/project2/depablo/erschultz'
 # dataDir='/home/erschultz/sequences_to_contact_maps'
 
