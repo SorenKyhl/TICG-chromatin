f83a8761d121dcafba4f048b359d0f970f528c64
Eric Schultz <erschultz@uchicago.edu>
2022-06-02 13:09:51 -0500

    path fix

--- bin/compare_contact.sh
+++ bin/compare_contact.sh
@@ -8,7 +8,8 @@
 #SBATCH --mem-per-cpu=2000
 
 
-dataFolder='/project2/depablo/erschultz/sequences_to_contact_maps/dataset_05_18_22'
+dataFolder='/project2/depablo/erschultz/dataset_05_18_22'
+# dataFolder='/home/erschultz/sequences_to_contact_maps/dataset_05_18_22'
 dir='/home/erschultz/sequences_to_contact_maps/dataset_04_27_22/samples/sample1'
 hatdir='/home/erschultz/sequences_to_contact_maps/dataset_04_27_22/samples/sample1/PCA-MSE/k10'
 
@@ -17,7 +18,7 @@ source activate python3.9_pytorch1.9_cuda10.2
 
 STARTTIME=$(date +%s)
 
-for sample in $(seq 1 18)
+for sample in $(seq 1 2)
 do
   echo $sample
   # python3 ~/TICG-chromatin/scripts/compare_contact.py --y "${dir}/y.npy" --y_diag "${dir}/y_diag.npy" --yhat "${hatdir}/y.npy" --yhat_diag "${hatdir}/y_diag.npy" --m=-1 --dir $hatdir
