c05b0bae70ee3b78f0afcbff1bef773eca34861d
Eric Schultz <erschultz@uchicago.edu>
2022-07-20 15:28:40 -0500

    update

--- scripts/import_contactmap_straw.py
+++ scripts/import_contactmap_straw.py
@@ -16,8 +16,8 @@ def download_contactmap_straw(filename, chrom, start, end, resolution):
     example input for knight-rubin (KR) normalized chromosome 2, 0-130Mbp at 100kbp resolution.
     raw_data = straw.straw("KR", filename, "2:0:129999999", "2:0:129999999", "BP", 100000)
     '''
-    basepairs = ":".join((str(chrom), str(start), str(end)))
-    raw_data = hicstraw.straw("NONE", filename, basepairs, basepairs, "BP", resolution)
+    basepairs = f"{chrom}:{start}:{end}"
+    raw_data = hicstraw.straw("observed", "NONE", filename, basepairs, basepairs, "BP", resolution)
     raw_data = np.array(raw_data)
     # raw data is a map between locus pairs and reads -- need to pivot into a matrix.
     df = pd.DataFrame(raw_data.transpose(), columns=['locus1 [bp]', 'locus2 [bp]', 'reads'])
@@ -38,7 +38,7 @@ def download_contactmap_straw(filename, chrom, start, end, resolution):
     return hic, xticks
 
 def import_contactmap_straw(sample_folder, filename, chrom=2, start=22000000, end=60575000, resolution=25000):
-    hic, xticks = download_contactmap_straw("https://hicfiles.s3.amazonaws.com/hiseq/gm12878/in-situ/combined.hic", chrom, start, end, resolution)
+    hic, _ = download_contactmap_straw("https://hicfiles.s3.amazonaws.com/hiseq/gm12878/in-situ/combined.hic", chrom, start, end, resolution)
     m, _ = hic.shape
 
     if not osp.exists(sample_folder):
@@ -77,7 +77,7 @@ def main():
         p.starmap(import_contactmap_straw, mapping)
 
 def main2():
-    dir = '/home/erschultz/sequences_to_contact_maps'
+    # dir = '/home/erschultz/sequences_to_contact_maps'
     dir = '/project2/depablo/erschultz'
     dataset='dataset_07_20_22'
     data_folder = osp.join(dir, dataset)
@@ -86,13 +86,14 @@ def main2():
     if not osp.exists(osp.join(data_folder, 'samples')):
         os.mkdir(osp.join(data_folder, 'samples'), mode = 0o755)
 
-    i = 1
     chromosome='4'
     start_mb=60
     resolution=5000
     filename="https://hicfiles.s3.amazonaws.com/hiseq/gm12878/in-situ/combined.hic"
-    filename='https://www.encodeproject.org/files/ENCFF718AWL/@@download/ENCFF718AWL.hic'
+    # filename='https://www.encodeproject.org/files/ENCFF718AWL/@@download/ENCFF718AWL.hic'
     hic = hicstraw.HiCFile(filename)
+    print(hic.getGenomeID())
+    print(hic.getResolutions())
     chr_mat = hic.getMatrixZoomData(chromosome, chromosome, "observed", "KR", "BP", resolution)
     for chrom in hic.getChromosomes():
         print(chrom.name, chrom.length)
@@ -113,6 +114,35 @@ def main2():
             f.write(f'{filename}\nchrom={chromosome}\nstart={start}\nend={end}\nresolution={resolution}\nbeads={m}')
 
         np.save(osp.join(sample_folder, 'y.npy'), y)
+
+def main3():
+    dir = '/home/erschultz/sequences_to_contact_maps'
+    # dir = '/project2/depablo/erschultz'
+    dataset='dataset_07_20_22'
+    data_folder = osp.join(dir, dataset)
+    if not osp.exists(data_folder):
+        os.mkdir(data_folder, mode = 0o755)
+    if not osp.exists(osp.join(data_folder, 'samples')):
+        os.mkdir(osp.join(data_folder, 'samples'), mode = 0o755)
+
+    chromosome='4'
+    start_mb=60
+    resolution=5000
+    filename="https://hicfiles.s3.amazonaws.com/hiseq/gm12878/in-situ/combined.hic"
+    filename='https://www.encodeproject.org/files/ENCFF718AWL/@@download/ENCFF718AWL.hic'
+
+    # set up for multiprocessing
+    mapping = []
+    for i, end_mb in enumerate([65, 70, 80, 90, 110, 130, 150, 190]):
+        start = start_mb * 10**6
+        end = end_mb * 10**6
+        m = (end - start) / resolution
+        print(i+1, start, end, m)
+        sample_folder = osp.join(data_folder, 'samples', f'sample{i+1}')
+        mapping.append((sample_folder, filename, chromosome, start, end, resolution))
+
+    with multiprocessing.Pool(10) as p:
+        p.starmap(import_contactmap_straw, mapping)
 
 
 if __name__ == '__main__':
