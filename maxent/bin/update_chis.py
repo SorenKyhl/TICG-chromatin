e9c595ece6ba29d9944e0cf3e545f4de4dd05ab9
Soren Kyhl <soren@kyhl.com>
2022-09-05 11:38:27 -0500

    restore update_chis and update_diag from bind branch, necessary for sk_run.sh

---
+++ maxent/bin/update_chis.py
@@ -0,0 +1,87 @@
+import argparse
+import json
+import os
+import os.path as osp
+import sys
+
+import numpy as np
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def getArgs():
+    parser = argparse.ArgumentParser(description='Base parser')
+    parser.add_argument('--it', type=int, help='current iteration')
+    parser.add_argument('--k', type=int, help='number of particle types')
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = getArgs()
+
+    config_file = "iteration{}/config.json".format(args.it)
+
+    with open(config_file, "r") as f:
+        config = json.load(f)
+
+    # read in current chis
+    with open('chis.txt', "r") as f_chis:
+        lines = f_chis.readlines()
+        current_chis = lines[args.it].split()
+        current_chis = [float(x) for x in current_chis]
+    # print("current chi values: ", current_chis)
+
+    counter = 0
+    for i in range(args.k):
+        for j in range(args.k):
+            if j < i:
+                continue
+            key = 'chi{}{}'.format(LETTERS[i], LETTERS[j])
+            try:
+                config[key] = current_chis[counter]
+            except IndexError as e:
+                print('Index Error')
+                print('lines:\n', lines)
+                print('current_chis:\n', current_chis)
+                print('k: ', args.k)
+                raise
+            counter += 1
+
+    with open(config_file, "w") as f:
+        json.dump(config, f)
+
+def test():
+    args = getArgs()
+    args.k = 1
+    args.it = 5
+
+    config = {}
+
+    allchis = np.atleast_2d(np.loadtxt('../sequences_to_contact_maps/dataset_08_26_21/samples/sample40/GNN-23/k1/chis.txt'))
+    print(allchis)
+    if len(allchis[0] == 0):
+        allchis = allchis.T
+        print(allchis.shape)
+
+    # get last row of 'chis.txt'
+    try:
+        lastchis = list(allchis[int(args.it)])
+        print(lastchis)
+    except IndexError as e:
+        print('Index Error')
+        print('allchis:\n', allchis)
+        raise
+
+    counter = 0
+    for i in range(args.k):
+        for j in range(args.k):
+            if j < i:
+                continue
+            key = 'chi{}{}'.format(LETTERS[i], LETTERS[j])
+            config[key] = lastchis[counter]
+            counter += 1
+
+    print(config)
+
+if __name__ == '__main__':
+    main()
---
+++ maxent/bin/update_diag.py
@@ -0,0 +1,53 @@
+import argparse
+import json
+import sys
+
+import numpy as np
+
+
+def getArgs():
+    parser = argparse.ArgumentParser(description='Base parser')
+    parser.add_argument('--it', type=int, help='current iteration')
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = getArgs()
+
+    config_file = "iteration{}/config.json".format(args.it)
+
+    with open(config_file) as f:
+        config = json.load(f)
+
+    #assert(config['diagonal_on'])
+    allchis = np.loadtxt('chis_diag.txt')
+
+    # get last row of 'chis.txt'
+    lastchis = list(allchis[int(args.it)])
+
+    config['diag_chis'] = lastchis
+
+    with open(config_file, "w") as f:
+        json.dump(config, f)
+
+if __name__ == '__main__':
+    main()
+
+
+"""
+if(config['plaid_on'] == True):
+    nspecies = config['nspecies']
+    ndiagchis = lastchis.size - nspecies*(nspecies+1)/2
+
+    if(ndiagchis>0):
+        config['diag_chis'] = lastchis[:-ndiagchis]
+
+        with open("config.json", "w") as f:
+            json.dump(config, f)
+else:
+    config['diag_chis'] = list(lastchis)
+
+    with open("config.json", "w") as f:
+        json.dump(config, f)
+"""
