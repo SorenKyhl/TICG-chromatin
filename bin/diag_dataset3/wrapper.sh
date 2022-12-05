671ef6f8aeb29066c808cf5ac340f0d2c90abe81
Eric Schultz <erschultz@uchicago.edu>
2022-12-05 10:17:59 -0600

    changes

--- bin/diag_dataset3/wrapper.sh
+++ bin/diag_dataset3/wrapper.sh
@@ -12,14 +12,14 @@ source activate python3.8_pytorch1.9
 cd ~/TICG-chromatin
 
 echo 'generate_params'
-python  ~/TICG-chromatin/bin/diag_dataset2/generate_params.py
+python  ~/TICG-chromatin/bin/diag_dataset3/generate_params.py
 mv ~/dataset_12_05_22 /project2/depablo/erschultz
 
 
-for i in 1 2 3 4 5 6 7 8
+for i in 1 2 3 4 5 6 7 8 9 10
 do
   echo $i
-  sbatch ~/TICG-chromatin/bin/diag_dataset2/diag_dataset${i}.sh &
+  sbatch ~/TICG-chromatin/bin/diag_dataset3/diag_dataset${i}.sh &
 done
 
 wait
