f140888ff4a54dc0a4e8910c4f594844294eaeae
Soren Kyhl <soren@kyhl.com>
2023-01-20 16:23:21 -0600

    change examples

--- examples/snippets/simulate_current_directory.py
+++ examples/snippets/simulate_current_directory.py
@@ -1,5 +1,10 @@
+import sys
 from pylib.pysim import Pysim
 
 if __name__ == "__main__":
     sim = Pysim.from_directory(".")
-    sim.run_eq(10000, 50000, 7)
+    if len(sys.argv) == 1:
+        sim.run_eq(10000, 50000, 7)
+    if len(sys.argv) == 2:
+        sim.run_eq(10000, sys.argv[1], 7)
+
