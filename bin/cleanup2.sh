167a5b505254de23e5d00a8e6063b7c519a138d0
Eric Schultz <erschultz@uchicago.edu>
2023-02-07 14:16:54 -0600

    job

--- bin/cleanup2.sh
+++ bin/cleanup2.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #SBATCH --job-name=cleanup2
 #SBATCH --output=logFiles/cleanup2.out
-#SBATCH --time=5:00:00'
+#SBATCH --time=5:00:00
 #SBATCH --account=pi-depablo
 #SBATCH --partition=depablo
 #SBATCH --ntasks=10
