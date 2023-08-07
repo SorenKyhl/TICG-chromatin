e437847350b69ff34b3c1ac0bcaaf33172e6c4fb
Eric Schultz <erschultz@uchicago.edu>
2023-08-07 12:41:37 -0500

    image

--- scripts/distances_Su2020/figures.py
+++ scripts/distances_Su2020/figures.py
@@ -394,8 +394,8 @@ def new_figure(sample, GNN_ID, bl=140, phi=0.03):
                 size=letter_fontsize, weight='bold')
 
 
-    plt.subplots_adjust(bottom=0.2, top = 0.9, left = 0.15, right = 0.95,
-                    hspace = 0.25, wspace = 0.25)
+    plt.subplots_adjust(bottom=0.25, top = 0.9, left = 0.1, right = 0.95,
+                    hspace = 0.25, wspace = 0.35)
     plt.tight_layout()
     plt.savefig('/home/erschultz/TICG-chromatin/figures/distances.png')
     plt.close()
