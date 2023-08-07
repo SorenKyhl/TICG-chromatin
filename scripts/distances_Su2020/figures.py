a3553489872987283df8708767868224d23f1c5a
Eric Schultz <erschultz@uchicago.edu>
2023-08-07 12:57:44 -0500

    image

--- scripts/distances_Su2020/figures.py
+++ scripts/distances_Su2020/figures.py
@@ -327,7 +327,7 @@ def new_figure(sample, GNN_ID, bl=140, phi=0.03):
     plt.figure(figsize=(18, 6))
     ax3 = plt.subplot(1, 24, (1, 6))
     ax4 = plt.subplot(1, 24, (8, 16)) # pc
-    ax5 = plt.subplot(1, 12, (10, 12)) # dist a-b
+    ax5 = plt.subplot(1, 24, (18, 24)) # dist a-b
 
     # plot scaling
     m = len(D)
@@ -394,8 +394,8 @@ def new_figure(sample, GNN_ID, bl=140, phi=0.03):
                 size=letter_fontsize, weight='bold')
 
 
-    plt.subplots_adjust(bottom=0.275, top = 0.9, left = 0.15, right = 0.925,
-                    hspace = 0.25, wspace = 0.25)
+    plt.subplots_adjust(bottom=0.275, top = 0.9, left = 0.15, right = 0.95,
+                    hspace = 0.25, wspace = 0.45)
     plt.tight_layout()
     plt.savefig('/home/erschultz/TICG-chromatin/figures/distances.png')
     plt.close()
