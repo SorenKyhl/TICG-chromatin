1e5e94b0dd3d46bc62cbeda52e57087ffe66ddea
Eric Schultz <erschultz@uchicago.edu>
2023-08-11 09:21:42 -0500

    change

--- scripts/distances_Su2020/figures.py
+++ scripts/distances_Su2020/figures.py
@@ -67,8 +67,8 @@ def old_figure(sample, GNN_ID, bl=140, phi=0.03):
     D_no_nan = D[~nan_rows][:, ~nan_rows] # ignore nan_rows
     D_pca = rescale_mu_sigma(D, D_pca)
     D_gnn = rescale_mu_sigma(D, D_gnn)
-    mu_D_pca, sigma_D_pca, mu_D, sigma_D = rescale_mu_sigma(D, D_pca)
-    mu_D_gnn, sigma_D_gnn, _, _ = rescale_mu_sigma(D, D_gnn)
+    mu_D_pca, sigma_D_pca, mu_D, sigma_D = rescale_mu_sigma(D, D_pca, True)
+    mu_D_gnn, sigma_D_gnn, _, _ = rescale_mu_sigma(D, D_gnn, True)
     # alpha_pca = min_MSE(D_no_nan, D_pca[~nan_rows][:, ~nan_rows])
     # alpha_gnn = min_MSE(D_no_nan, D_gnn[~nan_rows][:, ~nan_rows])
     alpha_pca = 1; alpha_gnn = 1
