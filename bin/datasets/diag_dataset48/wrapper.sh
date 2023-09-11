19654080d836d0ae602ba478b9dab50f37bbc69c
Eric Schultz <erschultz@uchicago.edu>
2023-09-11 13:10:28 -0500

    jobs

--- bin/datasets/diag_dataset48/wrapper.sh
+++ bin/datasets/diag_dataset48/wrapper.sh
@@ -23,7 +23,7 @@ cd ~/TICG-chromatin
 dataset="dataset_09_10_23"
 echo "generate_params for ${dataset}"
 # uses poly6 fit to max ent params + grid
-python  ~/TICG-chromatin/bin/datasets/generate_params.py --samples 5000 --b 261 --phi 0.01 --k 10 --m 512 --dataset $dataset --seq_mode 'none' --plaid_mode 'none' --diag_mode 'meanDist_S_grid' --data_dir '/project2/depablo/erschultz'
+# python  ~/TICG-chromatin/bin/datasets/generate_params.py --samples 5000 --b 261 --phi 0.01 --k 10 --m 512 --dataset $dataset --seq_mode 'none' --plaid_mode 'none' --diag_mode 'meanDist_S_grid' --data_dir '/project2/depablo/erschultz'
 #
 # cd "/home/erschultz/${dataset}"
 # tar -czvf setup.tar.gz setup
@@ -33,9 +33,9 @@ python  ~/TICG-chromatin/bin/datasets/generate_params.py --samples 5000 --b 261 
 # rm -r samples
 # # #
 sourceFile=~/TICG-chromatin/bin/datasets/diag_dataset48/diag_dataset_fns.sh
-# for i in 1 2 3 4 5
-#  # 6 7 8 9 10
-# do
-#   echo $i
-#   sbatch ~/TICG-chromatin/bin/datasets/diag_dataset${i}.sh $sourceFile 24
-# done
+for i in 1 2 3 4 5
+ # 6 7 8 9 10
+do
+  echo $i
+  sbatch ~/TICG-chromatin/bin/datasets/diag_dataset${i}.sh $sourceFile 24
+done
